b69bf82ae864f63d778e036f562fba02df602619
Rajat Surana <suranarajat07@gmail.com>
2026-08-18 22:09:26 +0530

    Time: 1568 ms (5.01%) | Memory: 59.8 MB (17.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,28 +3,43 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        int n = nums.length;
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<n-2;i++){
-            if(i> 0 && nums[i] == nums[i-1]){continue;}
+        // Set<List<Integer>> nu = new HashSet<>();
+        //     int n = nums.length;
 
-            int l = i+1; int r = n-1;
-            while(l<r){
-                int sum = nums[i] + nums[l] +nums[r];
+        //     for(int i=0;i<n-2;i++){
+        //         for(int j=i+1;j<n-1;j++){
+        //             for(int k=j+1;k<n;k++){
+        //                 if(nums[i] + nums[j] + nums[k] == 0){
+        //                      List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+        //                     Collections.sort(temp);
+        //                     nu.add(temp);
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     return new ArrayList<>(nu);
+
+
+
+        Set<List<Integer>> nu = new HashSet<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        int j=0;int k=0;
+        for(int i=0;i<n-2;i++){
+            j = i+1;  k =n-1;
+
+            while(j<k){
+                int sum =nums[i] +nums[j] + nums[k];
                 if(sum == 0){
-                    ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    while(l<r && nums[l] == nums[l+1]){l++;}
-                    while(r>l && nums[r] == nums[r-1]){r--;}
-                    l++;
-                    r--;
+                    // List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    //         Collections.sort(temp);
+                            nu.add(Arrays.asList(nums[i],nums[j],nums[k]));
                 }
-                else if(sum<0){l++;}
-                else{r--;}
+                if(sum<0){j++;}
+                else{k--;}
             }
         }
-        return ans;
-
+    return new ArrayList<>(nu);
         }
     
 }
