81142e311893b294a58cfc14bc64023e36dcdf8d
Rajat Surana <suranarajat07@gmail.com>
2026-08-27 19:30:24 +0530

    Time: 98 ms (5.03%) | Memory: 46.2 MB (50.66%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,20 +1,39 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> freqs = new HashMap<>();
-        int res = 0, i = 0, maxFreq = 0;
+        int low = 0;
+        int high = 0;
+        int res = Integer.MIN_VALUE;
+        int[] f = new int[256]; 
+        for(high = 0; high<s.length();high++){
+            f[s.charAt(high)]++;
 
-        for (int j = 0; j < s.length(); j++) {
-            char c = s.charAt(j);
-            freqs.put(c, freqs.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, freqs.get(c));
-            while ((j - i + 1) - maxFreq > k) {
-                char left = s.charAt(i);
-                freqs.put(left, freqs.get(left) - 1);
-                i++;
+            int mc = find(f);
+            int len = high-low+1;
+            int diff = len- mc;
+
+            while(diff>k){
+                f[s.charAt(low)]--;
+                low++;
+
+                mc = find(f);
+                len = high-low+1;
+                diff = len-mc;
             }
 
-            res = Math.max(res, j - i + 1);   
+            res = Math.max(res,high-low+1);
+
+             
         }
         return res;
+    }
+
+    int find(int[] a){
+        int maC = -1;
+
+        for(int i = 0; i<256;i++){
+            maC = Math.max(maC,a[i]);
+        }
+
+        return maC;
     }
 }
