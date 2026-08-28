66bc0d5cf5d6bf996777cfff477f9fd17b4fb5fd
Rajat Surana <suranarajat07@gmail.com>
2026-08-28 19:13:35 +0530

    Time: 2 ms (99.80%) | Memory: 45.5 MB (82.36%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,41 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
-            return "";
-        }
+
         int[] need = new int[128];
         for (char c : t.toCharArray()) {
             need[c]++;
         }
-        int left = 0;
-        int right = 0;
-        int required = t.length();
 
-        int minLen = Integer.MAX_VALUE;
+        int l = 0;
+        int r = 0;
+        int req = t.length();
+        int ml = Integer.MAX_VALUE;
         int start = 0;
 
-         while (right < s.length()) {
-            char c = s.charAt(right);
-            if (need[c] > 0) {
-                required--;
-            }
+        while (r < s.length()) {
+        char c = s.charAt(r);
+         if (need[c] > 0) {
+           req--;
+          }
 
-            need[c]--;
-            right++;
-            while (required == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    start = left;
+         need[c]--;
+         r++;
+
+         while (req == 0) {
+
+           if (r - l < ml) {
+                    ml = r - l;
+                    start = l;
+              }
+            char lC = s.charAt(l);
+                need[lC]++;
+
+                if (need[lC] > 0) {
+                    req++;
                 }
 
-                char leftChar = s.charAt(left);
-                need[leftChar]++;
-                if (need[leftChar] > 0) {
-                    required++;
-                }
-                left++;
+                l++;
             }
-         }
-         return minLen == Integer.MAX_VALUE? "" : s.substring(start, start + minLen);
+        }
+        return ml == Integer.MAX_VALUE ? "" : s.substring(start, start + ml);
     }
 }
